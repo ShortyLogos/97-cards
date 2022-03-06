379a6b09306e29450f6493bfac893f685fb7a843
Déric Marchand <dericmarchand@gmail.com>
2022-03-06 15:08:14 -0500

    missing SQL, end activity and stuck/end game trigger

--- app/src/main/java/com/dm/tpfinal/WelcomeActivity.java
+++ app/src/main/java/com/dm/tpfinal/WelcomeActivity.java
@@ -2,13 +2,33 @@ package com.dm.tpfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button btnJouer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        btnJouer = findViewById(R.id.btnJouer);
+
+        Ecouteur ec = new Ecouteur();
+
+        btnJouer.setOnClickListener(ec);
+    }
+
+    private class Ecouteur implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
+            startActivity(i);
+        }
     }
 }
